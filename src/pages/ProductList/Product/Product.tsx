8a1cc0f6c5f2abe3d9ce1939d6fbf7b7668ea06b
TonNQ <nguyenquoctoan12122003@gmail.com>
2023-08-26 21:02:37 +0700

    complete UI product card

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
+import ProductRating from 'src/components/ProductRating'
 import { Product as ProductType } from 'src/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
 
 interface Props {
   product: ProductType
@@ -23,54 +25,22 @@ export default function Product({ product }: Props) {
           <div className='flex items-center mt-3'>
             <div className='line-through max-w-[50%] text-gray-500 truncate'>
               <span className='text-xs'>₫</span>
-              <span className='text-sm'>{product.price_before_discount}</span>
+              <span className='text-sm'>
+                {formatCurrency(product.price_before_discount)}
+              </span>
             </div>
             <div className='text-orange truncate ml-1'>
               <span className='text-xs'>₫</span>
-              <span className='text-base font-medium'>{product.price}</span>
+              <span className='text-base font-medium'>
+                {formatCurrency(product.price)}
+              </span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div
-                  className='absolute top-0 left-0 h-full overflow-hidden'
-                  style={{ width: '50%' }}
-                >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='#ffce3d'
-                    className='w-3 h-3  fill-yellow'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
-                    />
-                  </svg>
-                </div>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='rgb(209 213 219)'
-                  className='w-3 h-3 fill-current text-gray-300'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-xs'>
               <span className='ml-1'>Đã bán </span>
-              <span>{product.sold}</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
             </div>
           </div>
         </div>
