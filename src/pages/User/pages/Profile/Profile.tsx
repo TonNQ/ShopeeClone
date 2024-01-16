7b06fe1d2846c0c4476916646ac8bd91a8bf2174
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-01-16 22:27:51 +0700

    handle profile form (yup validate, show information)

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,6 +1,62 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import userApi from 'src/apis/user.api'
+import Button from 'src/components/Button'
 import Input from 'src/components/Input'
+import InputNumber from 'src/components/InputNumber'
+import { UserSchema, userSchema } from 'src/utils/rules'
+
+type FormData = Pick<
+  UserSchema,
+  'name' | 'address' | 'avatar' | 'phone' | 'date_of_birth'
+>
+
+const profileSchema = userSchema.pick([
+  'name',
+  'address',
+  'avatar',
+  'phone',
+  'date_of_birth'
+])
 
 export default function Profile() {
+  const {
+    register,
+    control,
+    formState: { errors },
+    setValue
+  } = useForm<FormData>({
+    defaultValues: {
+      name: '',
+      address: '',
+      avatar: '',
+      phone: '',
+      date_of_birth: new Date(1990, 0, 1)
+    },
+    resolver: yupResolver<FormData>(profileSchema)
+  })
+  const { data: profileData } = useQuery({
+    queryKey: ['profile'],
+    queryFn: userApi.getProfile
+  })
+  const profile = profileData?.data.data
+  useEffect(() => {
+    if (profile) {
+      setValue('name', profile.name)
+      setValue('address', profile.address)
+      setValue('avatar', profile.avatar)
+      setValue('phone', profile.phone)
+      setValue(
+        'date_of_birth',
+        profile.date_of_birth
+          ? new Date(profile.date_of_birth)
+          : new Date(1990, 0, 1)
+      )
+    }
+  }, [profile, setValue])
+
   return (
     <div className='rounded-sm bg-white px-4 pb-10 shadow md:px-7 md:pb-20'>
       <div className='border-b border-b-gray-200 py-6'>
@@ -11,14 +67,14 @@ export default function Profile() {
           Quản lý thông tin hồ sơ để bảo mật tài khoản
         </div>
       </div>
-      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
-        <form className='mt-6 flex-grow md:mt-0 md:pr-12'>
+      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
           <div className='flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
               Email
             </div>
             <div className='sm:w-[80%] sm:pl-5'>
-              <div className='pt-3 text-gray-700'>abc123*****@gmail.com</div>
+              <div className='pt-3 text-gray-700'>{profile?.email}</div>
             </div>
           </div>
           <div className='mt-7 flex flex-col flex-wrap sm:flex-row'>
@@ -26,7 +82,13 @@ export default function Profile() {
               Tên
             </div>
             <div className='sm:w-[80%] sm:pl-5'>
-              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                register={register}
+                name='name'
+                placeholder='Tên'
+                errorMessage={errors.name?.message}
+              />
             </div>
           </div>
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
@@ -34,7 +96,21 @@ export default function Profile() {
               Số điện thoại
             </div>
             <div className='sm:w-[80%] sm:pl-5'>
-              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+              <Controller
+                control={control}
+                name='phone'
+                render={({ field }) => {
+                  return (
+                    <InputNumber
+                      classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                      placeholder='Số điện thoại'
+                      errorMessage={errors.phone?.message}
+                      {...field}
+                      onChange={field.onChange}
+                    />
+                  )
+                }}
+              />
             </div>
           </div>
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
@@ -42,7 +118,13 @@ export default function Profile() {
               Địa chỉ
             </div>
             <div className='sm:w-[80%] sm:pl-5'>
-              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                register={register}
+                name='address'
+                placeholder='Địa chỉ'
+                errorMessage={errors.address?.message}
+              />
             </div>
           </div>
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
@@ -63,7 +145,18 @@ export default function Profile() {
               </div>
             </div>
           </div>
-        </form>
+          <div className='mt-7 flex flex-col flex-wrap sm:flex-row'>
+            <div className='mb-2 truncate pt-3 capitalize sm:mb-0 sm:w-[20%] sm:text-right'></div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Button
+                className='flex h-9 items-center bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
+                type='submit'
+              >
+                Lưu
+              </Button>
+            </div>
+          </div>
+        </div>
         <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
           <div className='flex flex-col items-center'>
             <div className='my-5 h-24 w-24'>
@@ -74,7 +167,10 @@ export default function Profile() {
               />
             </div>
             <input type='file' accept='.jpg,.jpeg,.png' className='hidden' />
-            <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm capitalize text-gray-600 shadow-sm'>
+            <button
+              className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm capitalize text-gray-600 shadow-sm'
+              type='button'
+            >
               Chọn ảnh
             </button>
             <div className='mt-3 text-gray-400'>
@@ -83,7 +179,7 @@ export default function Profile() {
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
