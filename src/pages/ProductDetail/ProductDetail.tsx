a0294d3bf432203d5fe8bff418f6b30930693dfc
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-02-06 11:41:54 +0700

    component ErrorBoundary to catch error

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -24,7 +24,8 @@ import path from 'src/constants/path'
 export default function ProductDetail() {
   const queryClient = useQueryClient()
   const [buyCount, setBuyCount] = useState(1)
-  const { nameId } = useParams()
+  const { nameId, productId } = useParams()
+  const a = productId.value
   const id = getIdFromNameId(nameId as string)
   const { data: productDetailData } = useQuery({
     queryKey: ['product', id],
