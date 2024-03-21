f4619ee5315f6b189c36fa12b421f56c51db757f
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-03-21 15:29:17 +0700

    install vitest and test function

---
+++ src/utils/__test__/utils.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { describe, it, expect } from 'vitest'
+import { isAxiosError, isAxiosUnprocessableEntityError } from '../utils'
+import { AxiosError, HttpStatusCode } from 'axios'
+
+describe('isAxiosError', () => {
+  it('isAxiosError should return boolean', () => {
+    expect(isAxiosError(new Error())).toBe(false)
+    expect(isAxiosError(new AxiosError())).toBe(true)
+  })
+})
+
+describe('isAxiosUnprocessableEntityError', () => {
+  it('isAxiosUnprocessableEntityError should return boolean', () => {
+    expect(isAxiosUnprocessableEntityError(new Error())).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.InternalServerError,
+          data: null
+        } as any)
+      )
+    ).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.UnprocessableEntity,
+          data: null
+        } as any)
+      )
+    ).toBe(true)
+  })
+})
